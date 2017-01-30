b0876afcd2bf21f0cf44dd7c3e50963d9459b30f
Enzo <enzomuhlinghaus@gmail.com>
2017-01-30 11:34:41 +0100

    Adding img's + conf gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
-    less = require('gulp-less'),
+    sass = require('gulp-sass'),
     uglify = require('gulp-uglify'),
     filter = require('gulp-filter'),
     useref = require('gulp-useref'),
@@ -14,27 +14,12 @@ var jsFilter = filter('**/*.js', {restore: true});
 var cssFilter = filter('**/*.css', {restore: true});
 
 
-// Compile LESS files from /less into /css
-gulp.task('less', function() {
-    // return gulp.src('less/agency.less')
-    //     .pipe(less())
-    //     .pipe(header(banner, { pkg: pkg }))
-    //     .pipe(gulp.dest('css'))
-    //     .pipe(browserSync.reload({
-    //         stream: true
-    //     }))
+// Compile SCSS files from /scss into /css
+gulp.task('sass', function() {
+    return gulp.src('css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css'));
 });
-
-// // Minify compiled CSS
-// gulp.task('minify-css', ['less'], function() {
-//     return gulp.src('css/*.css')
-//         .pipe(cleanCSS({ compatibility: 'ie8' }))
-//         .pipe(rename({ suffix: '.min' }))
-//         .pipe(gulp.dest('dist/css'));
-//         // .pipe(browserSync.reload({
-//         //     stream: true
-//         // }))
-// });
 
 // Clean dist folder
 gulp.task('clean', function () {
@@ -42,23 +27,13 @@ gulp.task('clean', function () {
         .pipe(clean())
 });
 
-// Minify JS
-// gulp.task('minify-js', function() {
-//     return gulp.src('js/*.js')
-//         .pipe(uglify())
-//         .pipe(header(banner, { pkg: pkg }))
-//         .pipe(rename({ suffix: '.min' }))
-//         .pipe(gulp.dest('js'));
-//     //     .pipe(browserSync.reload({
-//     //         stream: true
-//     //     }))
-// });
-
 // Run everything
-gulp.task('default', ['less', 'clean'], function () {
+gulp.task('default', ['sass', 'clean'], function () {
     gulp.src('img/*.*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
+    gulp.src('fonts/*.*')
+        .pipe(gulp.dest('dist/fonts'));
     return gulp.src('*.html')
         .pipe(useref())
         // Minify Js
@@ -74,6 +49,7 @@ gulp.task('default', ['less', 'clean'], function () {
 
 // Check events
 gulp.task('watch', function () {
+    gulp.watch('css/*.scss', ['sass']);
     gulp.watch(['*.html', 'js/*.js', 'css/*.css']).on('change', function (event) {
         console.log(event);
         livereload.changed(event.path);
